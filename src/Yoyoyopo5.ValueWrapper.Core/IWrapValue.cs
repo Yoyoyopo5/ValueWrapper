6e8097c55ba73a31dd80b92e6da1985b2772f5f8
Yoyoyopo5 <118655115+Yoyoyopo5@users.noreply.github.com>
2026-05-05 00:42:13 -0500

    Add shared project with core types from ValueWrapper project. #9

---
+++ src/Yoyoyopo5.ValueWrapper.Core/IWrapValue.cs
@@ -0,0 +1,21 @@
+﻿namespace Yoyoyopo5.ValueWrapper;
+
+/// <summary>
+/// Implemented automatically by source generator when using <see cref="WrapperAttribute{T}"/>.
+/// </summary>
+/// <remarks>
+/// This interface enables the use of the <see cref="WrapperJsonConverter{TWrapper, TWrapped}"/>.
+/// </remarks>
+internal interface IWrapValue<T, out TWrapper>
+{
+    /// <summary>
+    /// The wrapped value.
+    /// </summary>
+    T Value { get; }
+    /// <summary>
+    /// Create an instance of the wrapper using the wrapped value.
+    /// </summary>
+    /// <param name="value">The value that should be wrapped.</param>
+    /// <returns>A new instance of <typeparamref name="TWrapper"/> wrapping <paramref name="value"/>.</returns>
+    static abstract TWrapper Create(T value);
+}
---
+++ src/Yoyoyopo5.ValueWrapper.Core/WrapperAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Yoyoyopo5.ValueWrapper;
+
+/// <summary>
+/// Indicates the type is a wrapper around another type.
+/// </summary>
+/// <remarks>
+/// This attribute informs the source generator to generate wrapper members for the type and add a JsonConverter.
+/// Mark the type with <see langword="partial"/> to use.
+/// </remarks>
+/// <typeparam name="T">The type to wrap.</typeparam>
+[AttributeUsage(validOn: AttributeTargets.Struct | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+internal class WrapperAttribute<T> : Attribute;
